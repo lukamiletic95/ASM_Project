e455e11db0c2c2a2ac6b3e9f1a79165e3a59c89f
lukamiletic95 <lukapn@gmail.com>
2019-01-16 11:44:16 -0800

    Actors' attributes added

--- actorsNetwork.py
+++ actorsNetwork.py
@@ -19,4 +19,52 @@ for actorList in sheet['Actors']:
                 G.add_edge(actors[i], actors[j])
                 G[actors[i]][actors[j]]['weight'] = 1
 
+
+actorsDictionary = {}
+
+for k in range(len(sheet['Actors'])):
+    actors = sheet['Actors'][k].split(",")
+    genres = sheet['Genre'][k].split(",")
+
+    for i in range(len(actors)):
+        actors[i] = actors[i].strip()
+
+    for i in range(len(genres)):
+        genres[i] = genres[i].strip()
+
+    for i in range(len(actors)):
+
+        if (not actors[i] in actorsDictionary):
+            actorsDictionary[actors[i]] = {}
+
+        for j in range(len(genres)):
+
+            if (genres[j] in actorsDictionary[actors[i]]):
+                actorsDictionary[actors[i]][genres[j]] = actorsDictionary[actors[i]][genres[j]] + 1
+            else:
+                actorsDictionary[actors[i]][genres[j]] = 1
+
+for actor in actorsDictionary.keys():
+    actorsDictionary[actor] = {k: v for k, v in
+                               sorted(actorsDictionary[actor].items(), key=lambda x: x[1], reverse=True)}
+
+for actor, genres in actorsDictionary.items():
+    topThreeGenres = ""
+
+    i = 0
+
+    for genreName in genres.keys():
+        topThreeGenres += genreName
+
+        if (i < 2):
+            topThreeGenres += ", "
+
+        i += 1
+
+        if (i == 3):
+            break
+
+    attributes = {actor: {'topThreeGenres': topThreeGenres}}
+    nx.set_node_attributes(G, attributes)
+
 nx.write_gml(G, "files/actorsNetwork.gml")
