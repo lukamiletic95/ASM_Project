81e24f30234fc55f6bc2408f8e60ef4c41821d0b
lukamiletic95 <lukapn@gmail.com>
2019-01-18 10:05:34 -0800

    Script to check communities

--- checkCommunities.py
+++ checkCommunities.py
@@ -5,7 +5,7 @@ import pandas as pd
 sheet = pd.read_csv(r"files/Communities.csv")
 communities = {}
 
-importantComunnities = [12, 24, 25, 6, 32]
+importantComunnities = [2, 0, 17, 8, 13]
 
 for k in range(len(sheet['Label'])):
     if importantComunnities.__contains__(sheet['modularity_class'][k]):
@@ -22,7 +22,7 @@ for k in range(len(sheet['Label'])):
 for comunity in communities.keys():
     communities[comunity] = {k: v for k, v in sorted(communities[comunity].items(), key=lambda x: x[1], reverse=True)}
 
-file = open("files/communitiesByGenre.txt", "w+")
+file = open("files/SmallCommunitiesByGenre.txt", "w+")
 
 for key, value in communities.items():
     file.write("*** " + str(key) + " ***\n")
